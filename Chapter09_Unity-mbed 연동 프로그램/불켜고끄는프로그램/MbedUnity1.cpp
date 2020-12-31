0c63dbb1378ea8a8c67b22d805102abb2cda74fc
DESKTOP-3SHNENU\홍영주 <abc123472000@naver.com>
2020-12-31 22:59:30 +0900

    수정

--- Chapter09_Unity-mbed 연동 프로그램/불켜고끄는프로그램/MbedUnity1.cpp
+++ Chapter09_Unity-mbed 연동 프로그램/불켜고끄는프로그램/MbedUnity1.cpp
@@ -1,22 +1,41 @@
-/*
-2020-12-30  UnityMbed 연동 프로그램1
-회로 : DigitalIn 회로
-UnityMbed1.cpp
- */
 #include "mbed.h"
 
 // 시리얼 통신
-Serial Pc(USBTX, USBRX);
+Serial pc(USBTX, USBRX);
 
-// PullDown 저항 방식을 사용하는 Button
-DigitalIn button(PD_14, PullDown);
+//Ticker 객체 생성
+Ticker myTicker;
 
-int main(){
+// 조도 센서를 A0 (PA_3)에 연결
+AnalogIn myCdS(A0);
 
-    while(1){
-        // 버튼을 누르면 방향 바뀌게하기
-        if(button == true){
-            printf("1");
-        }
+
+//조도값 저장 변수
+float brightness = 0.0f;
+
+void Processing()
+{
+
+    //조도 측정값을 변수에 저장
+    brightness = myCdS.read();
+
+    //어두우면 1을 전송 (Unity에서 1을 받으면 가상환경의 조명이 켜짐)
+    if (brightness < 0.3f)
+    {
+        pc.printf("1");
     }
+    else //밝으면 0을 전송 (Unity에서 0을 받으면 가상환경의 조명이 꺼짐)
+    {
+        pc.printf("0");
+    }
+
+}
+
+int main()
+{
+
+    //myTicker에 콜백 함수 등록 (주기 0.01초)
+    myTicker.attach(&Processing, 0.01);
+
+    while (1){    }
 }
