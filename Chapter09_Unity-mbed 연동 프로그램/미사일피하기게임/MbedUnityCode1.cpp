2cba8e5c460543d09a8904a61140b58c7378f2ea
DESKTOP-3SHNENU\홍영주 <abc123472000@naver.com>
2021-01-01 14:43:38 +0900

    수정

--- Chapter09_Unity-mbed 연동 프로그램/미사일피하기게임/MbedUnityCode1.cpp
+++ Chapter09_Unity-mbed 연동 프로그램/미사일피하기게임/MbedUnityCode1.cpp
@@ -1,5 +1,5 @@
 /*
-2020-12-28    Mbed-Unity 연동 프로그램 (로터리 엔코더 모듈 사용)
+2020-01-01    Mbed-Unity 연동 프로그램 (로터리 엔코더 모듈 사용)
 회로 : page 222
 MbedUnityCode1.cpp
  */
@@ -10,28 +10,33 @@ MbedUnityCode1.cpp
 Serial pc(USBTX, USBRX);
 // PA_5 핀으로 LED 제어
 DigitalOut LED(PA_5);
-// 이벤트 큐
-EventQueue *queue = mbed_event_queue();
 
 // 로터리 인코더(S1핀, S2핀, Key핀)
 mRotaryEncoder encoder(D0,D1,D2);
 int data;
 
-// 로터리 인코더 회전을 할 때 Unity에게 값을 전송 (#(숫자)$)형식으로 전달
-void print(){
-   pc.printf("#%d$\n", encoder.Get());
-}
-
 // 로터리 인코더를 회전할 때 호출되는 함수
 void rotate(){
-    queue->call(&print);
+    int data = encoder.Get();
+
+    if(data <0) {
+        pc.putc(0);
+        encoder.Set(0);
+    }
+    // 로터리 인코더의 한바퀴는 40
+    else if(data < 40){
+        pc.putc(data/2);
+    }
+    else {
+        pc.putc(20);
+        encoder.Set(40);
+    }
 }
 
 int main(){
     LED = false;
     // 회전되었을 때 호출되는 콜백함수 설정
     encoder.attachROT(&rotate);
-    queue->dispatch_forever();
 
     while(true){
     }
