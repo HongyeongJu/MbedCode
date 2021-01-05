8d802503e5fb7f690ec6a456eefc3e03b59a4b62
DESKTOP-3SHNENU\홍영주 <abc123472000@naver.com>
2021-01-05 17:04:58 +0900

    수정

--- Chapter15_VehicleHaptic/Vehiclehaptic_modify.cpp
+++ Chapter15_VehicleHaptic/Vehiclehaptic_modify.cpp
@@ -17,11 +17,9 @@ Ticker flipper1;
 
 // 샘플의 진폭을 저장하는 변수
 float buffer[BUFFER_SIZE];
-// 주파수당 샘플의 출력 시간을 저장하는 변수 ( 만약 인덱스가 0이면 100 Hz 변수가 100이면 (100 +100) Hz의 시간을 계산)
-uint16_t frequencies[200];
-int base_frequency =100;            // 기본 주파수가 100 Hz
 
-int hz_frequency = 0;
+// 처음 초기 frequency는 1 Hz
+int hz_frequency = 1;
 int i = 0;
 
 // 사인파 계산 함수 (BUFFER_SIZE개의 샘플의 전압(진폭)을 계산하는 함수)
@@ -33,11 +31,8 @@ void calculate_sinewave(void) {
 }
 
 // 사인파의 주기를 계산하는 함수(micro second단위)
-void calculate_frequency() {
-    // 100 + (0~ 200) Hz의 주파수 값을 출력하기 위한 사인파에서 한 샘플을 출력할 때의 시간 계산
-    for (int i = 0; i < 200; i++) {
-        frequencies[i] = 1000000 / (base_frequency + i) * BUFFER_SIZE;
-    }
+uint16_t calculate_frequency(int hz) {
+    return 1000000 / (hz * BUFFER_SIZE);
 }
 
 // Ticker를 사용해서 사인파 생성함수
@@ -48,10 +43,9 @@ void flip1() {
 
 int main() {
     calculate_sinewave();
-    calculate_frequency();
 
-    // 처음에는 100 Hz대역으로 사인파 생성
-    flipper1.attach_us(&flip1, frequencies[0]);
+    // 처음에는 hz_frequency Hz 대역으로 주파수 신호 출력
+    flipper1.attach_us(&flip1, calculate_frequency(hz_frequency));
 
 
     while (true) {}
